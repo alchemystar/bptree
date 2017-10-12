da4487201769519a27b4afb8af290171dcf9ce04
lizhuyang <lizhuyang@baidu.com>
2017-10-12 17:55:04 +0800

    fix test unit bug

--- src/main/java/alchemystar/bp/BTreeTest.java
+++ src/main/java/alchemystar/bp/BTreeTest.java
@@ -1,5 +1,7 @@
 package alchemystar.bp;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.junit.Test;
@@ -16,7 +18,7 @@ public class BTreeTest {
     @Test
     public void test() {
         BPTree bpTree = new BPTree();
-        int insertSize = 10000;
+        int insertSize = 100000;
         for (int i = 1; i <= insertSize; i++) {
             Value[] values = new Value[2];
             // Random random = new Random();
@@ -30,50 +32,18 @@ public class BTreeTest {
                 bpTree.insert(tuple);
             }
         }
-
-       /* Tuple t1 = genTuple(3);
-        Tuple t2 = genTuple(5);
-        Tuple t3 = genTuple(6);
-        Tuple t4 = genTuple(8);
-        Tuple t5 = genTuple(9);
-        Tuple t6 = genTuple(1);
-        Tuple t7 = genTuple(10);
-        Tuple t8 = genTuple(4);
-        Tuple t9 = genTuple(7);
-        Tuple t10 = genTuple(2);
-        Tuple t11 = genTuple(11);
-        //   Tuple tupleFive = genTuple(5);
-        bpTree.remove(t1);
-        bpTree.remove(t2);
-        bpTree.remove(t3);
-        bpTree.remove(t4);
-        bpTree.remove(t5);
-        //   bpTree.remove(tupleFive);
-        bpTree.remove(t6);
-        bpTree.remove(t7);
-        bpTree.remove(t8);
-        bpTree.remove(t9);
-        bpTree.remove(t10);
-        bpTree.remove(t11);*/
-        for (int i = 0; i < insertSize; i++) {
-            Random random = new Random();
-            int toInsert = random.nextInt(insertSize);
-            Tuple tuple = genTuple(toInsert);
-            bpTree.remove(tuple);
-        }
-      /*  BPNode node = bpTree.getHead();
-        // int sum = 0;
+        BPNode node = bpTree.getHead();
+        List<Tuple> list = new ArrayList<Tuple>();
         while (node != null) {
             for (int i = 0; i < node.getEntries().size(); i++) {
-                // System.out.println(node.getEntries().get(i));
-                Tuple tuple = bpTree.get(node.getEntries().get(i));
-                if (tuple == null) {
-                    System.out.println("it is null");
-                }
+                list.add(node.getEntries().get(i));
             }
-            node = node.getNext();
-        }*/
+            node = node.next;
+        }
 
+        for (Tuple item : list) {
+            bpTree.remove(item);
+        }
         printBtree(bpTree.getRoot());
     }
 
@@ -191,7 +161,7 @@ public class BTreeTest {
     @Test
     public void test3() {
         BPTree bpTree = new BPTree();
-        int insertSize = 10000;
+        int insertSize = 1000;
         for (int i = 1; i <= insertSize; i++) {
             Random random = new Random();
             int toInsert = random.nextInt(insertSize);
@@ -220,23 +190,17 @@ public class BTreeTest {
             printBtree(bpTree.getRoot());
         }
 
-        BPNode BPNode = bpTree.getHead();
-
-        while (BPNode.getNext() != null) {
-            BPNode = BPNode.getNext();
-        }
-        while (BPNode != null) {
-            for (int i = BPNode.getEntries().size() - 1; i >= 0; i--) {
-                // System.out.println(BPNode.getEntries().get(i));
-                Tuple tuple = bpTree.get(BPNode.getEntries().get(i));
-                bpTree.remove(tuple);
-                if (tuple == null) {
-                    System.out.println("it is null");
-                } else {
-                    System.out.println(tuple.getValues()[0]);
-                }
+        BPNode node = bpTree.getHead();
+        List<Tuple> list = new ArrayList<Tuple>();
+        while (node != null) {
+            for (int i = 0; i < node.getEntries().size(); i++) {
+                list.add(node.getEntries().get(i));
             }
-            BPNode = BPNode.getPrevious();
+            node = node.next;
+        }
+
+        for (Tuple item : list) {
+            bpTree.remove(item);
         }
         printBtree(bpTree.getRoot());
 
